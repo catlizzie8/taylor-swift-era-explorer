a35e9eb1ffa584c26b307b7977b9ccc613c02101
catlizzie8 <ntgdevice@gmail.com>
2024-08-09 14:43:57 -0400

    20240809

--- scripts.js
+++ scripts.js
@@ -2,19 +2,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('header');
   const sections = document.querySelectorAll('.Eras > div'); // Directly target the divs inside .Eras
   const fyiSection = document.querySelector('.fyi');
+  const links = document.querySelectorAll('header a');
+
+  links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      // Determine the offset based on the browser width
+      let headerHeight = window.innerWidth < 468 ? 150 : 200;
+      headerHeight = window.innerWidth < 550 ? 180 : 200;
+      const offsetTop = targetElement.offsetTop - headerHeight; // Subtract the appropriate header height
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
 
   const setHeaderColor = (sectionClass) => {
     const colorVariable = getComputedStyle(document.documentElement).getPropertyValue(`--${sectionClass}`);
     header.style.backgroundColor = colorVariable;
-
+  
     if (sectionClass === 'ttpd') {
       header.style.color = 'black';
       header.style.borderBottom = '2px solid black';
-    } else if (sectionClass === '_1989' || sectionClass === 'folklore') {
+      links.forEach(link => link.style.color = 'black');
+    } else if (sectionClass === 'folklore' || sectionClass === '_1989') {
       header.style.color = 'black';
+      links.forEach(link => link.style.color = 'black');
     } else {
       header.style.color = 'white';
       header.style.borderBottom = 'none';
+      links.forEach(link => link.style.color = 'white');
     }
   };
 
