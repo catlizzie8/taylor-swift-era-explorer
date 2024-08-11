2ab5f1cda4107155b7ce1d14f4e328fb21fcbfa8
catlizzie8 <ntgdevice@gmail.com>
2024-08-11 10:19:13 -0400

    20240811

--- scripts.js
+++ scripts.js
@@ -3,6 +3,43 @@ document.addEventListener('DOMContentLoaded', () => {
   const sections = document.querySelectorAll('.Eras > div');
   const fyiSection = document.querySelector('.fyi');
   const links = document.querySelectorAll('header a');
+  const toggleMusicButton = document.getElementById('toggleMusicButton');
+  const debutAudio = document.getElementById('debut-audio');
+  let wantedAudio = ""
+
+  const setHeaderColor = (sectionClass) => {
+    const colorVariable = getComputedStyle(document.documentElement).getPropertyValue(`--${sectionClass}`);
+    header.style.backgroundColor = colorVariable;
+    wantedAudio = sectionClass+"Audio"
+    const linkColor = (sectionClass === 'ttpd' || sectionClass === '_1989' || sectionClass === 'folklore') ? 'black' : 'white';
+    header.style.color = linkColor;
+    header.style.borderBottom = (sectionClass === 'ttpd') ? '2px solid black' : 'none';
+    links.forEach(link => link.style.color = linkColor);
+  };
+
+  const sectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const sectionClass = Array.from(entry.target.classList).find(cls => !['album', 'Info', 'songs', 'text'].includes(cls));
+        setHeaderColor(sectionClass);
+        if (sectionClass === 'debut') {
+          wantedAudio = debutAudio;
+        }
+      }
+    });
+  }, { threshold: 0.3 });
+
+  toggleMusicButton.addEventListener('click', () => {
+    if (wantedAudio.paused) {
+      wantedAudio.play();
+      toggleMusicButton.textContent = 'Turn Music Off';
+    } else {
+      wantedAudio.pause();
+      toggleMusicButton.textContent = 'Turn Music On';
+    }
+  });
+
+  sections.forEach(section => sectionObserver.observe(section));
 
   links.forEach(link => {
     link.addEventListener('click', (event) => {
@@ -19,26 +56,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  const setHeaderColor = (sectionClass) => {
-    const colorVariable = getComputedStyle(document.documentElement).getPropertyValue(`--${sectionClass}`);
-    header.style.backgroundColor = colorVariable;
-    const linkColor = (sectionClass === 'ttpd' || sectionClass === '_1989' || sectionClass === 'folklore') ? 'black' : 'white';
-    header.style.color = linkColor;
-    header.style.borderBottom = (sectionClass === 'ttpd') ? '2px solid black' : 'none';
-    links.forEach(link => link.style.color = linkColor);
-  };
-
-  const sectionObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const sectionClass = Array.from(entry.target.classList).find(cls => !['album', 'Info', 'songs', 'text'].includes(cls));
-        setHeaderColor(sectionClass);
-      }
-    });
-  }, { threshold: 0.4 });
-
-  sections.forEach(section => sectionObserver.observe(section));
-
   const fyiObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -46,6 +63,4 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }, { threshold: 0.1 });
-
-  fyiObserver.observe(fyiSection);
 });
